8871ffeb795bb3f12da39accd2b2fa26b67982ba
kiplangatsang <kiplangatsang425@gmail.com>
2023-05-16 04:55:29 +0300

    ads script

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -22,6 +22,8 @@
 				<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 				<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
 								crossorigin="anonymous"></script>
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
+								crossorigin="anonymous"></script>
 				{{--  @vite(['resources/sass/app.scss', 'resources/js/app.js'])  --}}
 </head>
 
