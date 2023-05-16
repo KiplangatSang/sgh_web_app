61ffd5ced3c577e36b24f82bad02fc12945700ea
kiplangatsang <kiplangatsang425@gmail.com>
2023-05-16 06:58:01 +0300

    ads

--- resources/views/post/poems.blade.php
+++ resources/views/post/poems.blade.php
@@ -94,34 +94,34 @@
 
 																</div>
 												</div>
-                                                <div class="row">
-                                                    <!-- google ads-->
-                                                    <div class="row">
-                                                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
-                                                                                    crossorigin="anonymous"></script>
-                                                                    <!-- responsive-square -->
-                                                                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9116569436922792"
-                                                                                    data-ad-slot="2577375845" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                                                                    <script>
-                                                                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                                                    </script>
-                                                    </div>
+												<div class="row">
+																<!-- google ads-->
+																<div class="row">
+																				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
+																								crossorigin="anonymous"></script>
+																				<!-- responsive-square -->
+																				<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9116569436922792"
+																								data-ad-slot="2577375845" data-ad-format="auto" data-full-width-responsive="true"></ins>
+																				<script>
+																								(adsbygoogle = window.adsbygoogle || []).push({});
+																				</script>
+																</div>
 
-                                    </div>
-                                    <div class="row">
-                                        <!-- google ads-->
-                                        <div class="row">
-                                                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
-                                                                        crossorigin="anonymous"></script>
-                                                        <!-- responsive-square -->
-                                                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9116569436922792"
-                                                                        data-ad-slot="2577375845" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                                                        <script>
-                                                                        (adsbygoogle = window.adsbygoogle || []).push({});
-                                                        </script>
-                                        </div>
+												</div>
+												<div class="row">
+																<!-- google ads-->
+																<div class="row">
+																				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
+																								crossorigin="anonymous"></script>
+																				<!-- responsive-square -->
+																				<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9116569436922792"
+																								data-ad-slot="2577375845" data-ad-format="auto" data-full-width-responsive="true"></ins>
+																				<script>
+																								(adsbygoogle = window.adsbygoogle || []).push({});
+																				</script>
+																</div>
 
-                        </div>
+												</div>
 								</div>
 				</div>
 @endsection
