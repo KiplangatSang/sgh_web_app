d6fa89e349d84dc170cb308821c2c55fc9296bc1
kiplangatsang <kiplangatsang425@gmail.com>
2023-05-16 06:28:54 +0300

    index ads

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -39,7 +39,7 @@
 																				<!-- Pager-->
 
 																</div>
-																<div class="col-md-4 col-lg-6 col-xl">
+																<div class="col-md-4 col-lg-5 col-xl">
 																				@if (count($data['recomended']) > 0)
 																								<h3 class="text-info">Recommended for you</h3>
 																								<div class="row">
@@ -56,17 +56,6 @@
 																								</div>
 																								<div class="mx-auto d-flex justify-content-center">
 																												{{ $data['recomended']->links() }}
-																								</div>
-
-																								<div class="col-md-2 col-xl-2 p-1">
-																												<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9116569436922792"
-																																crossorigin="anonymous"></script>
-																												<!-- responsive-square -->
-																												<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9116569436922792"
-																																data-ad-slot="2577375845" data-ad-format="auto" data-full-width-responsive="true"></ins>
-																												<script>
-																																(adsbygoogle = window.adsbygoogle || []).push({});
-																												</script>
 																								</div>
 																				@else
 																								<div class="jumbotron p-3">
