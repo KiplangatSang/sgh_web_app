5c6b9ef7f9db86cc6d1d70993e38894aa56fa3ca
kiplangatsang <kiplangatsang425@gmail.com>
2023-05-21 11:27:28 +0300

    posttitle

--- resources/views/post/inc/recomended.blade.php
+++ resources/views/post/inc/recomended.blade.php
@@ -1,6 +1,6 @@
 <div class="post-preview">
 				<a href="/{{ $recomended->post_id }}">
-								<h6 class="post-subtitle">{!! $recomended->title !!}</h6>
+
 								<h6 class="post-subtitle">{!! $recomended->post_subtitle !!}</h6>
 								<div class="row">
 												@if ($recomended['post_top_image'] != null)
@@ -9,7 +9,7 @@
 																@endforeach
 												@endif
 								</div>
-
+								<h5 class="post-subtitle">{!! $recomended->post_title !!}</h5>
 								<p class="meta">
 												<small>{{ $recomended->created_at->diffForHumans() }}</small>
 												{{--  <small>{{ $recomended->created_at->format('M d Y  H:II') }}</small>  --}}
