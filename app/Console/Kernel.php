16e9c5281343a68678a9f17a0fbcbeb9ceffc83c
kiplangatsang <kiplangatsang425@gmail.com>
2023-05-25 09:55:27 +0300

    three hour sitemap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->everyMinute();
-        $schedule->command('sitemap:generate')->everyMinute();
+        $schedule->command('sitemap:generate')->everyThreeHours();
     }
 
     /**
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
